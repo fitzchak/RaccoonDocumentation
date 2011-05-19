6ef113b9a13e71976fc21fe1ec36ffbbb6d91cae
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-19 12:53:53 +0300

    Use AutoMapper to map view model

--- src/RaccoonDocumentation.Web/Controllers/DocumentationController.cs
+++ src/RaccoonDocumentation.Web/Controllers/DocumentationController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using RaccoonDocumentation.Web.Infrastructure.AutoMapper;
 using RaccoonDocumentation.Web.Services;
 using RaccoonDocumentation.Web.ViewModels;
 
@@ -13,8 +14,7 @@ namespace RaccoonDocumentation.Web.Controllers
 			if (documentationItem == null)
 				return HttpNotFound("Slug not exist");
 
-			var model = new DocumentationPageViewModel();
-			model.Content = documentationItem.Content;
+			var model = documentationItem.MapTo<DocumentationPageViewModel>();
 			return View(model);
 		}
 	}
