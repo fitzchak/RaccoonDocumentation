201255c13386857b4d3dd1f1e3817f27e446e7d6
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-19 12:43:56 +0300

    Render the documentation content to the page, and return 404 if slug not found

--- src/RaccoonDocumentation.Web/Controllers/DocumentationController.cs
+++ src/RaccoonDocumentation.Web/Controllers/DocumentationController.cs
@@ -10,8 +10,11 @@ namespace RaccoonDocumentation.Web.Controllers
 		{
 			var documentationResolver = new DocumentationResolver();
 			var documentationItem = documentationResolver.Resolve(slug);
+			if (documentationItem == null)
+				return HttpNotFound("Slug not exist");
 
 			var model = new DocumentationPageViewModel();
+			model.Content = documentationItem.Content;
 			return View(model);
 		}
 	}
