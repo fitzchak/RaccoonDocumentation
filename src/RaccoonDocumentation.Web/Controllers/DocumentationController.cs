832b4418626ce78f3e5cb65c6f504ef39721e5cb
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-19 09:50:48 +0300

    Add DocumentationController

---
+++ src/RaccoonDocumentation.Web/Controllers/DocumentationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RaccoonDocumentation.Web.Controllers
+{
+	public class DocumentationController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
